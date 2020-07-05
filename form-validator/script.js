66f3dec2d5b3662f4c3e18640c1d13ae091d2f0e
nklaene <nicholasklaene@gmail.com>
2020-07-05 17:32:47 -0400

    did the entire project over again. kept html & css to a minimum, abstracted all js into functions instead of the mess I had before

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,50 +1,49 @@
-const form = document.querySelector('#signup')
+const form = document.querySelector("#signup");
+const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const passwordPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+const usernamePattern = /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // check email
+  checkField(emailPattern, form.email);
 
-    e.preventDefault();
-    checkEmail(form.email.value);
-    checkUsername(form.username.value);
-    checkPassword(form.password.value);
-    checkPasswordMatch(form.password.value, form.passwordRepeat.value);
+  // check username
+  checkField(usernamePattern, form.username);
 
-})
+  // check password
+  checkField(passwordPattern, form.password);
 
-// could abstract these all into a single function that takes two parameters: regex and field, but this is a small project so performance is not much of a factor
+  // check password match
+  checkPasswords(form.password, form.password2);
+});
 
-function checkEmail(email) {
-    const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    if (email.match(regex)) {
-        form.email.classList.add('success');
-    } else {
-        form.email.classList.remove('success');
-        form.email.classList.add('error');
-    }
-}
+const checkField = (pattern, element) => {
+  if (pattern.test(element.value)) {
+    addSuccess(element);
+  } else {
+    addError(element);
+  }
+};
 
-function checkUsername(username) {
-    if(username.match(/[a-zA-Z]{6,12}/)){
-        form.username.classList.add('success');
-    } else {
-        form.username.classList.remove('success');
-        form.username.classList.add('error');
-    }
-}
+const checkPasswords = (pass1, pass2) => {
+  if (pass1.value === pass2.value) {
+    addSuccess(pass2);
+  } else {
+    addError(pass2);
+  }
+};
 
-function checkPassword(password) {
-    if(password.match(/[a-zA-Z]{6,12}/)){
-        form.password.classList.add('success');
-    } else {
-        form.password.classList.add('error');
-        form.password.classList.remove('success');
-    }
-}
+const addSuccess = element => {
+  if (element.classList.contains("error")) {
+    element.classList.remove("error");
+  }
+  element.classList.add("success");
+};
 
-function checkPasswordMatch(pass1, pass2) {
-    if (pass1 === pass2) {
-        form.passwordRepeat.classList.add('success');
-    } else {
-        form.passwordRepeat.classList.remove('success');
-        form.passwordRepeat.classList.add('error');
-    }
-}
+const addError = element => {
+  if (element.classList.contains("success")) {
+    element.classList.remove("success");
+  }
+  element.classList.add("error");
+};
